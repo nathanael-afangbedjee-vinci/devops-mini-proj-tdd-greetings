5802022f9c53cf0317a5ce43159d28ba65a94bd6
nathanael <nathanael.afangbedjee@student.vinci.be>
2023-11-16 19:38:24 +0100

    test 2 sucessful

--- greet.js
+++ greet.js
@@ -1,4 +1,8 @@
 function greet(name) {
+        
+        if (name === null  || name === undefined || name.trim() === '') 
+        return "Hello, my friend.";
+
     return `Hello, ${(name)}.`;
 }
 
