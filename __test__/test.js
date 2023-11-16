c87b10e4dfb30125e754390640ee16c30d41e8af
nathanael <nathanael.afangbedjee@student.vinci.be>
2023-11-16 19:37:45 +0100

    test 2

--- __test__/test.js
+++ __test__/test.js
@@ -3,3 +3,15 @@ const helloWorld = require("../greet");
 test('returns: Hello, Bob given: Bob', () => {
     expect(helloWorld("Bob")).toMatch("Hello, Bob.");
 });
+
+test('returns: Hello, my friend given: null', () => {
+    expect(helloWorld(null)).toMatch("Hello, my friend.");
+});
+
+test('returns: Hello, my friend. given: undefined', () => {
+    expect(helloWorld(undefined)).toBe('Hello, my friend.')
+  });
+  
+test('return: Hello, my friend. given: "  " ', () => {
+    expect(helloWorld("  ")).toBe('Hello, my friend.')
+});
