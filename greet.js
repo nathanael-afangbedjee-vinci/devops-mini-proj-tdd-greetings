26e6666755fe51a3770cbe3d2b3fc4bba3a5e5d5
lopesTiago <tiagoandre.amaral@student.vinci.be>
2023-11-16 19:42:30 +0100

    test3 sucessful

--- greet.js
+++ greet.js
@@ -3,6 +3,9 @@ function greet(name) {
         if (name === null  || name === undefined || name.trim() === '') 
         return "Hello, my friend.";
 
+        if(name === name.toUpperCase())
+         return `HELLO, ${name}!`;
+
     return `Hello, ${(name)}.`;
 }
 
