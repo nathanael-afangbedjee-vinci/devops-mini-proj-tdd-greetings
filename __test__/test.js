d037f7c206479d52fff852e1ed8b69814dd56620
nathanael <nathanael.afangbedjee@student.vinci.be>
2023-12-14 18:53:07 +0100

    fixing many eslint error

--- __test__/test.js
+++ __test__/test.js
@@ -1,38 +1,45 @@
 const helloWorld = require("../greet");
 
-test('returns: Hello, Bob given: Bob', () => {
-    expect(helloWorld("Bob")).toMatch("Hello, Bob.");
+test("returns: Hello, Bob given: Bob", () => {
+  expect(helloWorld("Bob")).toMatch("Hello, Bob.");
 });
 
-test('returns: Hello, my friend given: null', () => {
-    expect(helloWorld(null)).toMatch("Hello, my friend.");
+test("returns: Hello, my friend given: null", () => {
+  expect(helloWorld(null)).toMatch("Hello, my friend.");
 });
 
-test('returns: Hello, my friend. given: undefined', () => {
-    expect(helloWorld(undefined)).toBe('Hello, my friend.')
-  });
-  
+test("returns: Hello, my friend. given: undefined", () => {
+  expect(helloWorld(undefined)).toBe("Hello, my friend.");
+});
+
 test('return: Hello, my friend. given: "  " ', () => {
-    expect(helloWorld("  ")).toBe('Hello, my friend.')
+  expect(helloWorld("  ")).toBe("Hello, my friend.");
 });
 
-test('return:  HELLO, KENOBI! given: KENOBI', () => {
-    expect(helloWorld("KENOBI")).toBe('HELLO, KENOBI!')
-  });
-
-test('giving Kratos, Thanathos given: Kratos and Thanathos.', () => {
-    expect(helloWorld(['Kratos', 'Thanathos'])).toBe('Hello, Kratos and Thanathos.');
+test("return:  HELLO, KENOBI! given: KENOBI", () => {
+  expect(helloWorld("KENOBI")).toBe("HELLO, KENOBI!");
 });
 
-test('return: Hello, Kratos, Thanathos and Hypnos. given: Kratos, Thanathos and Hypnos', () => {
-    expect(helloWorld(['Kratos', 'Thanathos' , 'Hypnos'])).toBe('Hello, Kratos, Thanathos and Hypnos.');
-  });
-  
-  test('return: Hello, Kratos, Thanathos, Protos and Hypnos. given: Kratos, Thanathos, Protos and Hypnos', () => {
-    expect(helloWorld(['Kratos', 'Thanathos' , 'Protos', 'Hypnos'])).toBe('Hello, Kratos, Thanathos, Protos and Hypnos.');
-  });
+test("giving Kratos, Thanathos given: Kratos and Thanathos.", () => {
+  expect(helloWorld(["Kratos", "Thanathos"])).toBe(
+    "Hello, Kratos and Thanathos.",
+  );
+});
 
-  test('return: Bonjour, Kratos et Thanathos. given: Kratos Thanathos in fr ', () => {
+test("return: Hello, Kratos, Thanathos and Hypnos. given: Kratos, Thanathos and Hypnos", () => {
+  expect(helloWorld(["Kratos", "Thanathos", "Hypnos"])).toBe(
+    "Hello, Kratos, Thanathos and Hypnos.",
+  );
+});
 
-    expect(helloWorld(['Kratos', 'Thanathos', 'fr'])).toBe('Bonjour, Kratos et Thanathos.');
+test("return: Hello, Kratos, Thanathos, Protos and Hypnos. given: Kratos, Thanathos, Protos and Hypnos", () => {
+  expect(helloWorld(["Kratos", "Thanathos", "Protos", "Hypnos"])).toBe(
+    "Hello, Kratos, Thanathos, Protos and Hypnos.",
+  );
+});
+
+test("return: Bonjour, Kratos et Thanathos. given: Kratos Thanathos in fr ", () => {
+  expect(helloWorld(["Kratos", "Thanathos", "fr"])).toBe(
+    "Bonjour, Kratos et Thanathos.",
+  );
 });
--- greet.js
+++ greet.js
@@ -1,97 +1,114 @@
-function greet(name, language = 'en') {
-    if (name && typeof name === 'object') {
-        return multipleWords(name, language);
-    }
+function greet(name, language = "en") {
+  if (name && typeof name === "object") {
+    return multipleWords(name, language);
+  }
 
-    if (name === null || name === undefined || name.trim() === '') {
-        return getGreeting('friend', language);
-    }
+  if (name === null || name === undefined || name.trim() === "") {
+    return getGreeting("friend", language);
+  }
 
-    if (name === name.toUpperCase()) {
-        return getUppercaseGreeting(name, language);
-    }
+  if (name === name.toUpperCase()) {
+    return getUppercaseGreeting(name, language);
+  }
 
-    return getNormalGreeting(name, language);
+  return getNormalGreeting(name, language);
 }
 
 function multipleWords(names, language) {
-    let lowerCaseNames = [];
-    let upperCaseNames = [];
-    let result = "";
+  let lowerCaseNames = [];
+  let upperCaseNames = [];
+  let result = "";
 
-    for (let i = 0; i < names.length; i++) {
-        if (names[i] === names[i].toUpperCase()) {
-            upperCaseNames.push(names[i]);
-        } else {
-            lowerCaseNames.push(names[i]);
-        }
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] === names[i].toUpperCase()) {
+      upperCaseNames.push(names[i]);
+    } else {
+      lowerCaseNames.push(names[i]);
     }
+  }
 
-    if (lowerCaseNames.length > 0) {
-        result += lowerCaseName(lowerCaseNames, language);
-    }
-    if (upperCaseNames.length > 0 && lowerCaseNames.length > 0) {
-        result += getConjunction(language) + ' ';
-    }
-    if (upperCaseNames.length > 0) {
-        result += upperCaseName(upperCaseNames, language);
-    }
-    return result;
+  if (lowerCaseNames.length > 0) {
+    result += lowerCaseName(lowerCaseNames, language);
+  }
+  if (upperCaseNames.length > 0 && lowerCaseNames.length > 0) {
+    result += getConjunction(language) + " ";
+  }
+  if (upperCaseNames.length > 0) {
+    result += upperCaseName(upperCaseNames, language);
+  }
+  return result;
 }
 
 function upperCaseName(upperCaseNames, language) {
-    let result = getUppercaseGreeting(upperCaseNames[0], language);
-    for (let i = 1; i < upperCaseNames.length - 1; i++) {
-        result += `, ${getUppercaseGreeting(upperCaseNames[i], language)}`;
-    }
-    result += upperCaseNames.length > 1 ? getConjunction(language) + ' ' + getUppercaseGreeting(upperCaseNames[upperCaseNames.length - 1], language) + '!' : '!';
-    return result;
+  let result = getUppercaseGreeting(upperCaseNames[0], language);
+  for (let i = 1; i < upperCaseNames.length - 1; i++) {
+    result += `, ${getUppercaseGreeting(upperCaseNames[i], language)}`;
+  }
+  result +=
+    upperCaseNames.length > 1
+      ? getConjunction(language) +
+        " " +
+        getUppercaseGreeting(
+          upperCaseNames[upperCaseNames.length - 1],
+          language,
+        ) +
+        "!"
+      : "!";
+  return result;
 }
 
 function lowerCaseName(lowerCaseNames, language) {
-    let result = getNormalGreeting(lowerCaseNames[0], language);
+  let result = getNormalGreeting(lowerCaseNames[0], language);
 
-    for (let i = 1; i < lowerCaseNames.length - 1; i++) {
-        result += `, ${getNormalGreeting(lowerCaseNames[i], language)}`;
-    }
-    result += lowerCaseNames.length > 1 ? getConjunction(language) + ' ' + getNormalGreeting(lowerCaseNames[lowerCaseNames.length - 1], language) + '.' : '.';
-    return result;
+  for (let i = 1; i < lowerCaseNames.length - 1; i++) {
+    result += `, ${getNormalGreeting(lowerCaseNames[i], language)}`;
+  }
+  result +=
+    lowerCaseNames.length > 1
+      ? getConjunction(language) +
+        " " +
+        getNormalGreeting(lowerCaseNames[lowerCaseNames.length - 1], language) +
+        "."
+      : ".";
+  return result;
 }
 
 function getNormalGreeting(name, language) {
-    const greetings = {
-        'en': `Hello, ${name}`,
-        'fr': `Bonjour, ${name}`,
-        'nl': `Hallo, ${name}`
-    };
-    return greetings[language] || greetings['en'];
+  const greetings = {
+    en: `Hello, ${name}`,
+    fr: `Bonjour, ${name}`,
+    nl: `Hallo, ${name}`,
+  };
+  return greetings[language] || greetings["en"];
 }
 
 function getUppercaseGreeting(name, language) {
-    const greetings = {
-        'en': `HELLO, ${name} `,
-        'fr': `SALUT, ${name} `,
-        'nl': `HALLO, ${name} `
-    };
-    return greetings[language] ? `${greetings[language]}!` : `${greetings['en']}!`;
+  const greetings = {
+    en: `HELLO, ${name} `,
+    fr: `SALUT, ${name} `,
+    nl: `HALLO, ${name} `,
+  };
+  return greetings[language]
+    ? `${greetings[language]}!`
+    : `${greetings["en"]}!`;
 }
 
 function getGreeting(type, language) {
-    const greetings = {
-        'en': `Hello, my ${type}.`,
-        'fr': `Bonjour, mon ${type}.`,
-        'nl': `Hallo, mijn ${type}.`
-    };
-    return greetings[language] || greetings['en'];
+  const greetings = {
+    en: `Hello, my ${type}.`,
+    fr: `Bonjour, mon ${type}.`,
+    nl: `Hallo, mijn ${type}.`,
+  };
+  return greetings[language] || greetings["en"];
 }
 
 function getConjunction(language) {
-    const conjunctions = {
-        'en': 'and',
-        'fr': 'et',
-        'nl': 'en'
-    };
-    return conjunctions[language] || conjunctions['en'];
+  const conjunctions = {
+    en: "and",
+    fr: "et",
+    nl: "en",
+  };
+  return conjunctions[language] || conjunctions["en"];
 }
 
 module.exports = greet;
